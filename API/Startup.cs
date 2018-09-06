bda51279d5c8b44fe4261112d1a25a6b2f630fca
mrmxs <maryzmaxs@gmail.com>
2018-09-06 22:29:31 +0300

    add CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,6 +22,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -36,8 +37,10 @@ namespace API
             {
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
+            app.UseCors(
+                options => options.WithOrigins(Configuration["Host"]).AllowAnyMethod()
+            );
+            
             app.UseMvc();
 
             var requiredVariables = new Dictionary<string, string>
